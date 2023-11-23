7ea606342fdab76cbe966c5b4898ddd38b709a23
serg <123456st@mail.ru>
2023-11-23 21:48:51 +0800

    dz3

--- dz1_app/urls.py
+++ dz1_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('about/', views.about, name='about'),
+    path('all_order/<int:client_id>/', views.all_order, name='all_order'),
+    path('all_product/<int:client_id>/', views.client_products, name='client_products'),
 ]
--- dz1_app/views.py
+++ dz1_app/views.py
@@ -1,5 +1,9 @@
+from datetime import timedelta, datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+
+from dz1_app.models import Order, Client
 
 
 def main(request):
@@ -34,3 +38,52 @@ def about(request):
                </html>
            """
     return HttpResponse(html_about)
+
+
+def all_order(request, client_id: int):
+    orders = Order.objects.filter(client_id=client_id)
+    context = {
+        'orders': orders,
+        # 'products': products,
+    }
+    print(orders)
+
+    return render(request, "dz1_app/all_order.html", context=context)
+
+
+def client_products(request, client_id):
+    end_date = datetime.now().date()
+    start_date_7_days_ago = end_date - timedelta(days=7)
+    start_date_30_days_ago = end_date - timedelta(days=30)
+    start_date_365_days_ago = end_date - timedelta(days=365)
+
+    products_last_7_days = get_client_products_in_date_range(client_id, start_date_7_days_ago, end_date)
+    products_last_30_days = get_client_products_in_date_range(client_id, start_date_30_days_ago, start_date_7_days_ago)
+    products_last_365_days = get_client_products_in_date_range(client_id, start_date_365_days_ago, start_date_30_days_ago)
+
+    client = Client.objects.get(id=client_id)
+
+    context = {
+
+        'client': client,
+        'products_last_7_days': products_last_7_days,
+        'products_last_30_days': products_last_30_days,
+        'products_last_365_days': products_last_365_days,
+    }
+
+    return render(request, 'dz1_app/client_products.html', context)
+
+def get_client_products_in_date_range(client_id, start_date, end_date):
+    orders = Order.objects.filter(client__id=client_id, order_date__range=[start_date, end_date])
+    print(orders)
+    print(start_date, end_date)
+    products_with_dates = []
+    for order in orders:
+        print(order)
+        for product in order.products.all():
+            print (product)
+            products_with_dates.append((product, order.order_date))
+            print(products_with_dates)
+            print('-----------------')
+
+    return products_with_dates
